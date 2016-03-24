f7310080fde4138ab4ff0582500b1ecba7b73271
rachell calhoun <rachell.calhoun@gmail.com>
2016-03-24 17:21:21 +0900

    add text safe

--- projects/models.py
+++ projects/models.py
@@ -15,7 +15,7 @@ class Skill(models.Model):
 	title = models.CharField(max_length=20)
 	link = models.CharField(max_length = 200,null=True, blank=True)
 	img = models.ImageField(null=True, blank=True)
-	text = models.TextField(default="Skill description")
+	text = tinymce_models.HTMLField()
 
 	def __str__(self):
 		return self.title
