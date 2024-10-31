f191ba4e5202c3e08e19530608cd7a459a0581a7
Rachell Calhoun <rachell.calhoun@gmail.com>
2024-10-31 15:47:42 -0400

    better link display

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from tinymce import models as tinymce_models
 class Project(models.Model):
 	title = models.CharField(max_length = 200)
 	published_date = models.DateTimeField(blank=True, null=True)
-	link = models.CharField(max_length = 200,null=True, blank=True)
+	link = models.URLField(max_length = 200,null=True, blank=True)
 	img = models.ImageField(null=True, blank=True)
 	text = tinymce_models.HTMLField()
 	def __str__(self):
